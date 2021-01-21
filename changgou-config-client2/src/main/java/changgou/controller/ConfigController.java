183c2da8ca70a35cb663ee5681010ff416a192a7
fanghaijie <fhjhaijie@163.com>
2021-01-21 22:47:06 +0800

    集成bus实现动态刷新配置,全局通知和定点通知

--- changgou-config-client2/src/main/java/changgou/controller/ConfigController.java
+++ changgou-config-client2/src/main/java/changgou/controller/ConfigController.java
@@ -1,4 +1,4 @@
-package com.changgou.controller;
+package changgou.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
